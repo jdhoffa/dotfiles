bd3edbe9bbb03c0169ca84bf21bd4d6abd5c969f
Jackson Hoffart <jackson.hoffart@gmail.com>
2023-01-23 18:49:22 +0100

    use auto indent

--- mackup/.config/nvim/lua/jdhoffa/opt.lua
+++ mackup/.config/nvim/lua/jdhoffa/opt.lua
@@ -29,7 +29,9 @@ vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
+vim.opt.autoindent = true
 vim.opt.smartindent = true
+vim.opt.breakindent = true
 
 -- folding
 vim.opt.foldmethod = "expr"
