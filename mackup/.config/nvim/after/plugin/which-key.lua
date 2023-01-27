a891c1cae282dd4b86a99d07bba3351b22fdd82b
Jackson Hoffart <jackson.hoffart@gmail.com>
2023-01-27 18:14:59 +0100

    use test all

--- mackup/.config/nvim/after/plugin/which-key.lua
+++ mackup/.config/nvim/after/plugin/which-key.lua
@@ -199,7 +199,7 @@ wk.register({
 
   r = {
     name = "R-language",
-    t = { '<cmd>SlimeSend0 "devtools::test()\\n"<cr>', "Test" },
+    t = { '<cmd>SlimeSend0 "devtools::test()\\n"<cr>', "Test all" },
     c = { '<cmd>SlimeSend0 "devtools::check()\\n"<cr>', "Check" },
     b = { '<cmd>SlimeSend0 "devtools::build()\\n"<cr>', "Build" },
     i = { '<cmd>SlimeSend0 "devtools::install()\\n"<cr>', "Install" },
