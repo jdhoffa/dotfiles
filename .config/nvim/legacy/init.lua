39d313d0d87993b075b8b0d663345e0c0b67fe18
Jackson Hoffart <jackson.hoffart@gmail.com>
2023-03-28 14:24:13 +0200

    remove legacy nvim config

--- .config/nvim/legacy/init.lua
+++
@@ -1,7 +0,0 @@
-require("jdhoffa.opt")
-require("jdhoffa.packer")
-require("jdhoffa.commands")
-require("jdhoffa.autocmd")
-require("jdhoffa.keymap")
--- TODO: Move this to a .lua setting in plugins
-vim.g.slime_target = 'tmux'
