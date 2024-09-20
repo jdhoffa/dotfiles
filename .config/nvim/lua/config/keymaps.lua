c69e62ce041593f5aad7f6533661f75306d18f0f
Jackson Hoffart <jackson.hoffart@gmail.com>
2024-09-20 14:17:31 +0200

    remove buffer keymaps

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -18,11 +18,3 @@ vim.keymap.set("n", "<leader>6", "<cmd>lua require('harpoon.ui').nav_file(6)<cr>
 vim.keymap.set("n", "<leader>7", "<cmd>lua require('harpoon.ui').nav_file(7)<cr>", { desc = "Harpoon Buffer 7" })
 vim.keymap.set("n", "<leader>8", "<cmd>lua require('harpoon.ui').nav_file(8)<cr>", { desc = "Harpoon Buffer 8" })
 vim.keymap.set("n", "<leader>9", "<cmd>lua require('harpoon.ui').nav_file(9)<cr>", { desc = "Harpoon Buffer 9" })
-
--- buffer
-vim.keymap.set("n", "<leader>bb", "<cmd>Telescope buffers<cr>", { desc = "Telescope" })
-vim.keymap.set("n", "<leader>bj", "<cmd>bn<cr>", { desc = "Next" })
-vim.keymap.set("n", "<leader>bk", "<cmd>bp<cr>", { desc = "Previous" })
-vim.keymap.set("n", "<leader>bn", "<cmd>bn<cr>", { desc = "Next" })
-vim.keymap.set("n", "<leader>bp", "<cmd>bp<cr>", { desc = "Previous" })
-vim.keymap.set("n", "<leader>bsd", "<cmd>%bd|e#|bd#<cr>|'<cr>", { desc = "Delete surrounding" })
