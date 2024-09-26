b582312b2755742b7fc206a41c7cd7f73ae6a29c
Jackson Hoffart <jackson.hoffart@gmail.com>
2024-09-26 15:00:30 +0200

    use tab for cmp

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -6,6 +6,22 @@ return {
   },
   opts = function(_, opts)
     local cmp = require("cmp")
+    opts.mapping["<CR>"] = nil
+    opts.mapping = vim.tbl_extend("force", opts.mapping, {
+      ["<Tab>"] = cmp.mapping(function(fallback)
+        -- This little snippet will confirm with tab, and if no entry is selected, will confirm the first item
+        if cmp.visible() then
+          local entry = cmp.get_selected_entry()
+          if not entry then
+            cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+          else
+            cmp.confirm()
+          end
+        else
+          fallback()
+        end
+      end, { "i", "s", "c" }),
+    })
     opts.sources = cmp.config.sources(vim.list_extend(opts.sources, { { name = "copilot" } }))
     opts.formatting = {
       fields = { "kind", "abbr", "menu" },
