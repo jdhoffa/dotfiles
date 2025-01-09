70425b12225f03cb00ec275bac9f241667e07365
Jackson Hoffart <jackson.hoffart@gmail.com>
2025-01-09 11:08:42 +0100

    update packages

--- .config/nvim/lua/plugins/harpoon.lua
+++ .config/nvim/lua/plugins/harpoon.lua
@@ -1,8 +1,7 @@
 return {
-  "ThePrimeagen/harpoon",
-  dependencies = "nvim-lua/plenary.nvim",
-  opts = {
-    global_settings = { mark_branch = true },
-    width = vim.api.nvim_win_get_width(0) - 4,
-  },
+    "ThePrimeagen/harpoon",
+    dependencies = "nvim-lua/plenary.nvim",
+    opts = {
+        global_settings = { mark_branch = true },
+    },
 }
