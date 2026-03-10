8c23314fa516413ce37e2244d8f7d04025dd4e68
Jackson Hoffart <jackson.hoffart@gmail.com>
2026-03-10 16:29:30 +0100

    migrate to fish v4

--- fish/.config/fish/conf.d/color.fish
+++ fish/.config/fish/conf.d/color.fish
@@ -1,7 +1,37 @@
-set -U fish_color_autosuggestion black
-set -U fish_color_command normal
-set -U fish_color_error red
-set -U fish_color_param cyan
-set -U fish_color_redirections yellow
-set -U fish_color_terminators white
-set -U fish_color_valid_path green
+set --global fish_color_autosuggestion black
+set --global fish_color_cancel -r
+set --global fish_color_command normal
+set --global fish_color_comment red
+set --global fish_color_cwd green
+set --global fish_color_cwd_root red
+set --global fish_color_end green
+set --global fish_color_error red
+set --global fish_color_escape brcyan
+set --global fish_color_history_current --bold
+set --global fish_color_host normal
+set --global fish_color_host_remote yellow
+set --global fish_color_normal normal
+set --global fish_color_operator brcyan
+set --global fish_color_param cyan
+set --global fish_color_quote yellow
+set --global fish_color_redirection cyan --bold
+set --global fish_color_redirections yellow
+set --global fish_color_search_match white --background=brblack --bold
+set --global fish_color_selection white --background=brblack --bold
+set --global fish_color_status red
+set --global fish_color_terminators white
+set --global fish_color_user brgreen
+set --global fish_color_valid_path green
+set --global fish_pager_color_background
+set --global fish_pager_color_completion normal
+set --global fish_pager_color_description yellow -i
+set --global fish_pager_color_prefix normal --bold --underline
+set --global fish_pager_color_progress brwhite --background=cyan --bold
+set --global fish_pager_color_secondary_background
+set --global fish_pager_color_secondary_completion
+set --global fish_pager_color_secondary_description
+set --global fish_pager_color_secondary_prefix
+set --global fish_pager_color_selected_background -r
+set --global fish_pager_color_selected_completion
+set --global fish_pager_color_selected_description
+set --global fish_pager_color_selected_prefix
